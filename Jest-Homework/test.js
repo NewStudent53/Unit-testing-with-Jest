9f8142de12a9410726c128d1a7795f6d40e8045a
Guillermo R <79545062+NewStudent53@users.noreply.github.com>
2023-06-24 01:03:24 -0400

    Add files via upload

---
+++ Jest-Homework/test.js
@@ -0,0 +1,29 @@
+// importar la función sum del archivo app.js
+const { sum } = require('./app.js');
+
+// comienza tu primera prueba
+test('adds 14 + 9 to equal 23', () => {
+    //dentro de la prueba llamamos a nuestra función sum con 2 números
+    let total = sum(14, 9);
+
+    // esperamos que la suma de esos 2 números sea 23
+    expect(total).toBe(23);
+});
+
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
+
+test('Yen_Test', function(){
+    const { fromDollarToYen } = require('./app.js')
+    expect(fromDollarToYen(3.5)).toBe(373.03); //1 dollar is 106.58 yen, then 3.5 dollars should be = (3.5 * 106.58)
+})
+
+test('Pound_Test', function(){
+    const { fromYenToPound } = require('./app.js')
+    expect(fromYenToPound(3.5)).toBe(0.021); //1 yen is 0.006 pounds, then 3.5 yen should be = (3.5 * 0.006)
+})
